094f614a16338bbc16f4a0c368d3a3b076d14fe8
Ryan Dana <147992254+ryandana@users.noreply.github.com>
2025-08-19 16:28:07 +0800

    Update cors.php

--- config/cors.php
+++ config/cors.php
@@ -6,7 +6,7 @@ return [
 
     'allowed_methods' => ['*'],
 
-    'allowed_origins' => ['http://localhost:3000'],
+    'allowed_origins' => ['https://cv-project-azure.vercel.app'],
 
     'allowed_origins_patterns' => [],
 
